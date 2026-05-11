5f6557d5f502748e0465eb65097c213c90c6eae4
Yuri1101 <62683858+Yuri1101@users.noreply.github.com>
2026-05-11 20:12:55 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const {
+  Client,
+  GatewayIntentBits
+} = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log("Bot起動");
+});
+
+client.on("messageCreate", async (message) => {
+
+  if (message.author.bot) return;
+
+  if (/^\d{5}$/.test(message.content)) {
+
+    const targetChannelId =
+      "ここにチャンネルID";
+
+    const channel =
+      await client.channels.fetch(targetChannelId);
+
+    await channel.setName(message.content);
+
+    message.reply("変更した！");
+  }
+});
+
+client.login(process.env.TOKEN);
