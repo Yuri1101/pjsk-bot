04b20a228edf8a61e5c18627dd4935d2466e8975
Yuri1101 <62683858+Yuri1101@users.noreply.github.com>
2026-05-11 20:40:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const {
 } = require("discord.js");
 
 const express = require("express");
+
 const app = express();
 
 app.get("/", (req, res) => {
@@ -30,17 +31,53 @@ client.on("messageCreate", async (message) => {
 
   if (message.author.bot) return;
 
+  // 監視するチャンネルだけ
+  const allowedChannelId =
+    "1487402528609013760";
+
+  if (message.channel.id !== allowedChannelId)
+    return;
+
+  // 5桁数字判定
   if (/^\d{5}$/.test(message.content)) {
 
-    const targetChannelId =
-      "1380947003407728780";
+    try {
 
-    const channel =
-      await client.channels.fetch(targetChannelId);
+      // VCチャンネルID
+      const vcChannelId =
+        "1380947003407728780";
 
-    await channel.setName(message.content);
+      // テキストチャンネルID
+      const textChannelId =
+        "1380946601077379183";
 
-    message.reply("変更したっ！");
+      const vcChannel =
+        await client.channels.fetch(vcChannelId);
+
+      const textChannel =
+        await client.channels.fetch(textChannelId);
+
+      // 名前変更
+      await vcChannel.setName(
+        `周回【${message.content}】`
+      );
+
+      await textChannel.setName(
+        `部屋番号【${message.content}】`
+      );
+
+      await message.reply(
+        `部屋番号 ${message.content} に変更したよっ！`
+      );
+
+    } catch (err) {
+
+      console.error(err);
+
+      await message.reply(
+        "失敗…"
+      );
+    }
   }
 });
 
